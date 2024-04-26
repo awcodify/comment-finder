93b7b04c7e4da823f87e58c7d2b1ea7979a1f988
awcodify <awcodify@gmail.com>
2024-04-26 11:03:55 +0700

    add docs for matching_authors

--- README.md
+++ README.md
@@ -45,3 +45,7 @@ jobs:
 - **keywords**: Comment keywords to search for (comma-separated). For instance:
   `approve,yes,LGTM`.
 - **token**: GitHub token.
+
+## Outputs
+
+- **matching_authors**: This will show authors which commented using given keyword in the PR.
--- src/main.js
+++ src/main.js
@@ -59,7 +59,7 @@ async function run() {
     core.debug(`keywords: ${keywords}`)
 
     if (keywordFoundInSomeComment) {
-      core.setOutput('mathing_author', matchingAuthors)
+      core.setOutput('matching_authors', matchingAuthors)
     } else {
       core.setFailed(
         `One of the required authors or team members must comment with the required keywords.`
